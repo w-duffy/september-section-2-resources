2bc84545adf80df49119e1661a24cf8a7b9e02d5
w-duffy <w.duffy@outlook.com>
2025-01-30 19:04:55 -0600

    adds w18d4

---
+++ 6-mod/18-week/4-day/seed-demo/README.md
@@ -0,0 +1,13 @@
+# Run the Seeder
+
+Out of shell:
+
+```bash
+pipenv run flask seed all
+```
+
+In shell:
+
+```bash
+flask seed all
+```
---
+++ 6-mod/18-week/4-day/seed-demo/app/models/users.py
@@ -0,0 +1,34 @@
+from .db import db
+
+likes = db.Table(
+    "likes",
+    db.Column("author_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("post_id", db.Integer, db.ForeignKey("posts.id"), primary_key=True),
+)
+
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    posts = db.relationship("Post", back_populates="author")
+    # posts = db.relationship("Post", back_populates="author", lazy="joined") # Eager loads the user's posts anytime you query for users; better to eagerly load on a per route basis
+    liked_posts = db.relationship(
+        "Post", secondary="likes", back_populates="user_likes"
+    )
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+    def to_dict_with_posts(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "Posts": [post.to_dict() for post in self.posts],
+        }
